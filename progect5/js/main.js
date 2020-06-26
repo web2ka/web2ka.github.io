efcdea21841c5bfb76c7a5f72b8fc4843cddb069
web2ka <web2ka@github.com>
2020-06-26 14:16:45 +0300

    update progect5

--- progect5/js/main.js
+++ progect5/js/main.js
@@ -3,7 +3,14 @@ $(document).ready(function(){
 
 $('.header__burger').click(function(event) {
 		$('.header__burger,.header__menu').toggleClass('active');
-		$('body').toggleClass('lock');
+
 	});
 
+$('.header__list').click(function(event) {
+        $('.header__burger,.header__menu').removeClass('active');
+
+        });
+
 });
+
+
