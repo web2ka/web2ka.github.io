f2d5cb0a0764702a0fbb1db374a8c7b28aa9278f
web2ka <65822322+web2ka@users.noreply.github.com>
2021-01-27 00:23:38 +0200

    додано фільтр аніматорів

--- progect17/js/script.js
+++ progect17/js/script.js
@@ -135,3 +135,30 @@ const swiper = new Swiper('.image-slider', {
 	}
 
 })
+
+//////filter
+
+$(function(){
+	let filter = $("[data-filter]");
+	filter.on("click", function(event) {
+		event.preventDefault();
+		let cat = $(this).data('filter');
+
+		if(cat == 'all') {
+			$("[data-cat]").removeClass("hide");
+		} else {
+			$("[data-cat]").each(function () {
+
+				let itemAnim = $(this).data('cat');
+
+				if (itemAnim != cat) {
+					$(this).addClass('hide');
+				} else {
+					$(this).removeClass('hide');
+				}
+
+			});
+		}
+
+	});
+});
