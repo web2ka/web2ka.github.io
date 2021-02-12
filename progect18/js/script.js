3e8f7e00541cc70a1b7ba658d7a2fcb1ba67469e
web2ka <65822322+web2ka@users.noreply.github.com>
2021-02-12 11:09:26 +0200

    додано новий проект

---
+++ progect18/js/script.js
@@ -0,0 +1,43 @@
+function testWebP(callback) {
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+	if (support == true) {
+		document.querySelector('html').classList.add('_webp');
+	} else {
+		document.querySelector('html').classList.add('_no-webp');
+	}
+});
+
+//menu
+
+let unlock = true;
+let burger = document.querySelector(".navbar__burger");
+if (burger != null) {
+	let deley = 500;
+	let menu = document.querySelector(".navbar__menu");
+	let body = document.querySelector("body");
+	burger.addEventListener("click", function (e) {
+		if (unlock) {
+			//body_lock(delay);
+			burger.classList.toggle("_active");
+			menu.classList.toggle("_active");
+			body.classList.toggle("_lock")
+		}
+	});
+	let list = document.querySelector(".navbar__list");
+	list.addEventListener("click", function (e) {
+		if (unlock) {
+			//body_lock(delay);
+			burger.classList.toggle("_active");
+			menu.classList.toggle("_active");
+			body.classList.toggle("_lock")
+		}
+	});
+};
+
+//end menu
