17a5c1217e18f14a389d123946349847bbf409c1
web2ka <65822322+web2ka@users.noreply.github.com>
2021-02-15 01:35:03 +0200

    зміна роботи слайдера

--- progect18/js/script.js
+++ progect18/js/script.js
@@ -42,6 +42,29 @@ if (burger != null) {
 
 //end menu
 
+//=================
+//Tabs
+let tabs = document.querySelectorAll("._tabs");
+for (let index = 0; index < tabs.length; index++) {
+	let tab = tabs[index];
+	let tabs_items = tab.querySelectorAll("._tabs-item");
+	let tabs_blocks = tab.querySelectorAll("._tabs-block");
+	for (let index = 0; index < tabs_items.length; index++) {
+		let tabs_item = tabs_items[index];
+		tabs_item.addEventListener("click", function (e) {
+			for (let index = 0; index < tabs_items.length; index++) {
+				let tabs_item = tabs_items[index];
+				tabs_item.classList.remove('_active');
+				tabs_blocks[index].classList.remove('_active');
+			}
+			tabs_item.classList.add('_active');
+			tabs_blocks[index].classList.add('_active');
+			e.preventDefault();
+		});
+	}
+}
+//=================
+
 //slider
 new Swiper('.swiper-container', {
 
